73ecdf9af4af42653c0b81d41a0df7701040bcf3
codebot053 <codebot053@gmail.com>
2022-11-08 13:18:37 +0900

    user.urls, views simple-jwt 를 활용한 회원가입 기능구현

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+from user import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('test/',views.TokenTestView.as_view(), name='test'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
-
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 # Create your views here.
+from .models import User
+from .serializers import UserSerializer
+
+
+class SignUpView(APIView):
+
+    def post(self, request):
+        data = request.data
+
+        qs = User.objects.filter(email=request.data['email']) # TODO: serializer 에서 처리
+        if qs.exists():
+            return Response({"msg":"email exists"}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "user": serializer.data,
+                    "message": "signup success",
+                    "token": {
+                        "access": access_token,
+                        "refresh": refresh_token,
+                    },
+
+                },
+                status=status.HTTP_201_CREATED
+            )
+            return res
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TokenTestView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({"msg":"test pass"}, status=status.HTTP_200_OK)
+
