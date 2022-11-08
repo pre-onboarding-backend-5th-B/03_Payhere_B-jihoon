5667d2e0d941642640374254cddc860a2376ea5a
codebot053 <codebot053@gmail.com>
2022-11-08 17:41:56 +0900

    modified: moneybook.serializers MoneyBookSerializer 삭제 MoneyBook 레코드를 시리얼라이저를 통해 생성하지 않고 user.serializers 에서 ORM을 사용하여 생성하기 위해 삭제

--- moneybook/serializers.py
+++ moneybook/serializers.py
@@ -2,4 +2,9 @@ from rest_framework import serializers
 
 from moneybook.models import MoneyBook, MoneyBookLog
 
+class MoneyBookSerializer(serializers.ModelSerializer):
 
+    class Meta:
+        model = MoneyBook
+        fields = '__all__'
+    
