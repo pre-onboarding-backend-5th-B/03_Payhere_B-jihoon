5c3c0ce4a8a54c73ec5e228a41bd9d1cd0430144
codebot053 <codebot053@gmail.com>
2022-11-10 11:23:30 +0900

    feat: 가계부 조회, 작성 기능 추가 MoneyBookLogAPIView 를 생성하여 /moneybook/ URI 에서 GET(유저의 가계부 기록 조회), POST(유저의 가계부 기록 생성)기능을 추가.

--- moneybook/views.py
+++ moneybook/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from moneybook.models import MoneyBook, MoneyBookLog
+from moneybook.serializers import MoneyBookLogCreateSerializer
 
 # Create your views here.
+
+
+class MoneyBookLogAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user_id = request.user.id
+        user_moneybook = MoneyBook.objects.get(user=user_id)
+        user_moneybook_log = MoneyBookLog.objects.filter(moneybook=user_moneybook.id)
+        print(user_moneybook_log)
+        return Response(
+            MoneyBookLogCreateSerializer(user_moneybook_log, many=True).data,
+            status=status.HTTP_200_OK,
+        )
+
+    def post(self, request):
+        user_id = request.user.id
+        user_moneybook = MoneyBook.objects.get(user=user_id)
+
+        log_id = user_moneybook.latest_log_id + 1
+
+        request_dic = request.data
+        request_dic["log_id"] = log_id
+        request_dic["moneybook"] = user_moneybook.id
+
+        serializer = MoneyBookLogCreateSerializer(data=request_dic)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            user_moneybook.latest_log_id += 1
+            user_moneybook.save()
+            return Response({"msg": serializer.data}, status=status.HTTP_201_CREATED)
